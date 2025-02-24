60a261278167eb994c297de72bf34a53db8b6329
Aaron Lin <shihsyul@uci.edu>
2025-02-23 16:00:42 -0800

    fixed search component bugs and injected spotify service into album page.

--- client/src/app/pages/album-page/album-page.component.ts
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
     selector: 'app-album-page',
@@ -16,11 +17,19 @@ export class AlbumPageComponent implements OnInit {
 	tracks:TrackData[];
 
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
 
   ngOnInit() {
     this.albumId = this.route.snapshot.paramMap.get('id');
     //TODO: inject spotifyService and use it to get the album data and the tracks for the album
+    this.spotifyService.getAlbum(this.albumId)
+      .then(album => {
+        this.album = album;
+      });
+    this.spotifyService.getTracksForAlbum(this.albumId)
+      .then(tracks => {
+        this.tracks = tracks;
+      });
   }
 
 }
