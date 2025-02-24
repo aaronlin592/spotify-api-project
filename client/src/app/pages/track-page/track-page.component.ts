1d9cec0df7046f10326984a209b9e1bf0f34c7df
Aaron Lin <shihsyul@uci.edu>
2025-02-24 00:12:45 -0800

    implemented carousel components and fixed track list duration function. fixed correspondingly to artist page and injected spotify into trackpage

--- client/src/app/pages/track-page/track-page.component.ts
+++ client/src/app/pages/track-page/track-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
     selector: 'app-track-page',
@@ -14,11 +15,15 @@ export class TrackPageComponent implements OnInit {
 	trackId:string;
 	track:TrackData;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
 
   ngOnInit() {
   	this.trackId = this.route.snapshot.paramMap.get('id');
   	//TODO: Inject the spotifyService and use it to get the track data
+    this.spotifyService.getTrack(this.trackId)
+      .then(track => {
+        this.track = track;
+      });
     
   }
 
